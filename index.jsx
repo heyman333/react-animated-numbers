1bc2dc094925711ca079e1c2e876395e6af51465
youngsoohan <amazingmobdev@gmail.com>
2021-08-01 15:26:23 +0900

    feat: upgrage module

--- index.jsx
+++ index.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React from "react";
 import { Spring, animated } from "react-spring";
 
 const NUMBERS = [
@@ -6,63 +6,39 @@ const NUMBERS = [
   6, 7, 8, 9,
 ];
 
-const usePrevious = (value) => {
-  const ref = React.useRef();
-  React.useEffect(() => {
-    ref.current = value;
-  });
-
-  if (typeof ref.current === "undefined") {
-    return 0;
-  }
-
-  return ref.current;
-};
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 const AnimatedNumber = ({
   animateToNumber,
   fontStyle,
-  config,
+  configs,
   includeComma,
-  delay,
   onFinish,
   onStart,
-  animationType = "random",
 }) => {
-  const prevNumber = usePrevious(animateToNumber);
   const animteTonumberString = String(Math.abs(animateToNumber));
-  const prevNumberString = String(Math.abs(prevNumber));
   const animateToNumbersArr = Array.from(animteTonumberString, Number);
-  const prevNumbersArr = Array.from(prevNumberString, Number);
   const [onStarted, setOnStarted] = React.useState(false);
   const [onFinished, setOnFinished] = React.useState(false);
 
   React.useEffect(() => {
     if (onStarted && onStart) {
-      const delayTime = typeof delay === "undefined" ? 0 : delay;
-
-      setTimeout(() => {
-        onStart();
-        setOnStarted(false);
-      }, delayTime);
-
-      if (onFinish && animationType === "random") {
-        const delayTime =
-          typeof config?.duration === "undefined" ? 500 : config.duration;
-        setTimeout(() => {
-          onFinish();
-        }, delayTime);
-      }
+      onStart();
+      setOnStarted(false);
     }
-  }, [onStarted]);
+    if (onFinished && onFinish) {
+      onFinish();
+      setOnFinished(false);
+    }
+  }, [onStarted, onFinished, onFinish, onStart]);
 
   if (includeComma) {
     const reducedArray = new Array(
       Math.ceil(animteTonumberString.length / 3)
-    ).fill(0);
-
-    const startReducedArray = new Array(
-      Math.ceil(prevNumberString.length / 3)
     ).fill(0);
 
     reducedArray.forEach((__, index) => {
@@ -76,40 +52,11 @@ const AnimatedNumber = ({
         ","
       );
     });
-
-    startReducedArray.forEach((__, index) => {
-      if (index === 0) {
-        return;
-      }
-
-      prevNumbersArr.splice(prevNumberString.length - index * 3, 0, ",");
-    });
   }
 
   const [numberHeight, setNumberHeight] = React.useState(0);
 
   const numberDivRef = React.useRef(null);
-
-  const getDelay = useCallback(
-    (index) => {
-      const sliced = animateToNumbersArr.slice(index);
-      const gap = sliced.filter((item) => typeof item === "string").length;
-
-      if (delay) {
-        return delay + (animateToNumbersArr.length - 1 - index - gap) * 200;
-      }
-
-      if (config && config.duration) {
-        return (
-          (animateToNumbersArr.length - 1 - index - gap) *
-          Math.max(100, config.duration - 300)
-        );
-      }
-
-      return (animateToNumbersArr.length - 1 - index - gap) * 200;
-    },
-    [animateToNumbersArr, onFinished]
-  );
 
   React.useEffect(() => {
     setNumberHeight(numberDivRef.current.clientHeight);
@@ -118,7 +65,10 @@ const AnimatedNumber = ({
   return (
     <>
       {numberHeight !== 0 && (
-        <div style={{ display: "flex", flexDirection: "row" }}>
+        <div
+          style={{ display: "flex", flexDirection: "row" }}
+          className="animated-container"
+        >
           {animateToNumber < 0 && <div style={fontStyle}>-</div>}
           {animateToNumbersArr.map((n, index) => {
             if (typeof n === "string") {
@@ -148,10 +98,13 @@ const AnimatedNumber = ({
                       numberHeight * 20
                     })`,
                   }}
-                  delay={animationType === "calm" ? getDelay(index) : null}
                   onRest={() => setOnFinished(true)}
                   onStart={() => setOnStarted(true)}
-                  config={config}
+                  config={
+                    configs
+                      ? configs[getRandomIntInclusive(0, configs.length - 1)]
+                      : undefined
+                  }
                 >
                   {(props) =>
                     NUMBERS.map((number, i) => (
