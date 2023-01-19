f948da1487965084bc20ef5b5584a2ca40aea4f9
John Gale <jgale@ancoris.com>
2023-01-19 13:23:53 +0000

    add type definitions for alternate configs

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,13 @@
 import React from "react";
 import { SpringConfig } from "react-spring";
 
+type configsFn = ({numberValue, index}: {numberValue: number, index: number}) => SpringConfig;
+
 export interface Props {
   animateToNumber: number;
   fontStyle?: React.CSSProperties;
   includeComma?: boolean;
-  configs?: SpringConfig[];
+  configs?: SpringConfig[] | configsFn;
 }
 
 declare const AnimatedNumber: React.FunctionComponent<Props>;
